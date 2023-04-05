672fe0484d8acaa1405b8c7a94b1bd947ca38eb7
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-05 22:36:16 +0900

    Made some changes with Home and Contact Me pages.

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -4,10 +4,8 @@ from send_email import send_email
 st.header("Contact Me")
 
 with st.form(key="form"):
-    email_address = st.text_input("Please enter your email address: "
-                                  "That does not automatically send you any confirmation "
-                                  "email but sends your email address to me as part of your message. "
-                                  "As soon as I know I received the message, I'll respond to you!")
+    email_address = st.text_input("Please enter your email address:")
+
     raw_message = st.text_area("Your message:")
     message = f"""\
     Subject: New email from {email_address}
@@ -15,6 +13,7 @@ with st.form(key="form"):
     From: {email_address}
     {raw_message}
     """
+
     submit_button = st.form_submit_button("Submit")
     print(submit_button)
 
@@ -22,3 +21,6 @@ with st.form(key="form"):
         send_email(message)
         st.info("Your email was sent successfully.")
 
+    st.write("If you have an error after pressing on the Submit button, "
+             "please directly send your message to ten87tak@gmail.com from "
+             "your email client.")
