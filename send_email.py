d966913a58079dc49ba5e74666f020c1d2654861
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-05 23:25:04 +0900

    Added some instructional comments in the send_email.py file.

--- send_email.py
+++ send_email.py
@@ -6,9 +6,13 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
+    # This is an environment variable. Enter your email address here.
     username = "EMAIL"
+
+    # This is an environment variable. Create an app password and type it in here.
     password = "PASSWORD"
 
+    # This is an environment variable. Enter your email address here.
     receiver = "EMAIL"
     context = ssl.create_default_context()
 
