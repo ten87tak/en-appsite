ade9dbf82b9985970d3e64f89011305d25315f77
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-10 00:04:32 +0900

    Modified Other_Apps.py for astro api app and added astro_pic.jpg file.

--- pages/Other_Apps.py
+++ pages/Other_Apps.py
@@ -6,20 +6,27 @@ column_1, column_2 = st.columns(2)
 
 with column_1:
     st.header("GEOMETRY GAME")
-
-    st.subheader("This is a CLI game where a player will be given random "
+    st.write("This is a CLI game where a player will be given random "
                  "coordinates that make two points, which produces a rectangle.")
-
-    st.subheader("The player is then asked to type in a set of X and Y coordinates "
+    st.write("The player is then asked to type in a set of X and Y coordinates "
                  "which they think is located within that rectangle. Also, the "
                  "player will be asked to enter an area they guess. ")
-
-    st.subheader("Finally, the program draws the rectangle and the guessed "
+    st.write("Finally, the program draws the rectangle and the guessed "
                  "point in the graphic window.")
+
+    st.write("")
+
+    st.image("images/astro_pic.jpg")
+    st.write("[Source Code](https://github.com/ten87tak/Astronomy_Today)")
 
 
 with column_2:
     st.image("images/OOP_App1.PNG")
-
     st.write("[Source Code](https://github.com/ten87tak/OOP_App1_GeoGame)")
+
+    st.write("")
+
+    st.header("Astronomy Image of the Day 🌏")
+    st.write("You can check out the astronomy picture of the day.")
+    st.write("This is created with the NASA API.")
 
