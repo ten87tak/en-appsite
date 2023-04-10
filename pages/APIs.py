64088863694adfa7ad61253bb3e686384c0e4ce1
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-10 23:39:43 +0900

    Added source code for Dictionary API.

--- pages/APIs.py
+++ pages/APIs.py
@@ -8,7 +8,7 @@ with column_1:
     st.title("Dictionary API")
     st.subheader("Run it in your terminal. Copy and paste the format in the "
                  "URL address bar, type in a word you like in place of 'word', "
-                 "and then you get the word and its definition in a dictionary!:)")
+                 "and then you get the word and its definition in a dictionary! :)")
 
 with column_3:
     st.write("")
@@ -16,3 +16,5 @@ with column_3:
     st.write("")
     st.image("images/dict_api_urlbar.PNG")
     st.image("images/dict_api.PNG")
+
+    st.write("[Source Code](https://github.com/ten87tak/Dictionary_API)")
