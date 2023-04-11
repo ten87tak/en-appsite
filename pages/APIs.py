a19891ac038c8ebc6a3a66d2018359a13d62bfef
tenandtak <114275420+tenandtak@users.noreply.github.com>
2023-04-11 23:35:11 +0900

    Made a minor change.

--- pages/APIs.py
+++ pages/APIs.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-column_1, column_2, column_3 = st.columns([5, 1, 6])
+column_1, column_2, column_3 = st.columns([5, 0.5, 6])
 
 with column_1:
     st.title("Dictionary API")
